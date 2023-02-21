f584865ea78209fd7ba95db314011766f3824c0f
sibelckya <94621439+sibelckya@users.noreply.github.com>
2023-02-21 15:28:12 +0100

    Add files via upload

---
+++ view/connexion.php
@@ -0,0 +1,9 @@
+<h1>Authentification</h1>
+<div><?= @$erreur ?></div>
+<form action="index.php?page=setAuth" method="post">
+
+    Email : <input type="email" name="email" class="input input-bordered input-primary w-full max-w-xs"><br>
+    Mdp : <input type="password" name="mdp" class="input input-bordered input-primary w-full max-w-xs"><br>
+    <button type="">S'identifier</button>
+
+</form>
