827e0b698670bec2f4c721ba3cc809da0c136bdf
KheiraLeila <kheiraddj@gmail.com>
2023-03-06 15:57:15 +0100

    modification du style

--- view/articleView.php
+++ view/articleView.php
@@ -1,9 +1,9 @@
-<div>
-    <h2><?=$article['titre']?></h2>
-        <img src='<?=$article['image']?>'>
-        <p><?=$article['contenu']?></p>
-        <p><?=$article['dateCreation']?></p>
+<div class="md:flex">
+    <h2 class="text-3xl font-bold dark:text-white text-center"><?=$article['titre']?></h2><br>
+        <img class="h-auto max-w-lg mx-auto" src='<?=$article['image']?>'>
+        <p class="text-center"><?=$article['contenu']?></p>
+        <p class="text-center"><?=$article['dateCreation']?></p>
         <?php if (@$_SESSION['id_role'] == 1) { ?>
-            <button class="btn btn-outline btn-error"><a href="?p=deleteArticle&id=<?= $article['id_article'] ?>">Supprimer</a></button> 
+            <button class="btn btn-outline btn-error content-center"><a href="?p=deleteArticle&id=<?= $article['id_article'] ?>">Supprimer</a></button> 
         <?php } ?>
     </div>
--- view/articlesView.php
+++ view/articlesView.php
@@ -1,11 +1,11 @@
-<body class="flex flex-col min-h-screen">
+<body class="flex flex-col min-h-screen md:flex">
    <div class="grid grid-cols-4 gap-4">  
 <?php foreach($articles as $article){?>
    
         <div>
-            <a href='?p=article&id=<?=$article['id_article']?>'><h2><?=$article['titre']?></h2>
+            <a href='?p=article&id=<?=$article['id_article']?>'>
+            <h1 class="text-3xl font-bold dark:text-white text-center"><?=$article['titre']?></h1>
             <img class="object-cover h-48 w-96 ..."src='<?=$article['image']?>'>
-            <p><?=$article['contenu']?></p>
             <p><?=$article['nom']?></p>
             <p><?=$article['dateCreation']?></p>
         </div>
